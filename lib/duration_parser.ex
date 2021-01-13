7eb9645c75dd2d8e5bfe39e803e194b0cbb164ba
Sergey Zubkov <ksevelyar@gmail.com>
2021-01-13 17:24:19 +0300

    refactor functions for better naming and error handling

--- lib/duration_parser.ex
+++ lib/duration_parser.ex
@@ -11,17 +11,29 @@ defmodule DurationParser do
   iex> DurationParser.parse_minutes("02:15")
   {:ok, 135}
 
+  iex> DurationParser.parse_minutes(":15")
+  {:error, "expected hours"}
+
   iex> DurationParser.parse_minutes("2h 35m")
   {:ok, 155}
 
   iex> DurationParser.parse_minutes("10")
   {:ok, 10}
 
+  iex> DurationParser.parse_minutes("10d")
+  {:error, "unknown format"}
+
   iex> DurationParser.parse_minutes("10h")
   {:ok, 600}
 
+  iex> DurationParser.parse_minutes("30m")
+  {:ok, 30}
+
   iex> DurationParser.parse_minutes("0.5h")
   {:ok, 30}
+
+  iex> DurationParser.parse_minutes("h 30m")
+  {:error, "expected hours"}
 
   iex> DurationParser.parse_minutes("0.5")
   {:ok, 30}
@@ -32,62 +44,81 @@ defmodule DurationParser do
   iex> DurationParser.parse_minutes("7.5")
   {:ok, 450}
 
+  iex> DurationParser.parse_minutes("7a.5")
+  {:error, "expected 2 digits"}
+
   iex> DurationParser.parse_minutes("24.5")
   {:ok, 1470}
 
-  # iex> DurationParser.parse_minutes("a24.5")
-  # {:error, "expected 2 digits"}
+  iex> DurationParser.parse_minutes("a24.5")
+  {:error, "expected 2 digits"}
   """
 
-  # def parse_minutes(<<head::binary-size(1), _tail::binary>>)  do
-  #   {:error, "expected 2 digits"}
-  # end
-
-  def parse_minutes(string) do
-    _parse_minutes(string, string)
+  def parse_minutes(duration_string) do
+    detect_format(duration_string, duration_string)
   end
 
-  defp _parse_minutes(<<head::binary-size(1), tail::binary>>, string) do
-    _parse_minutes(head, tail, string)
+  defp convert_to_minutes(hours_string) do
+    case hours_string |> String.trim() |> Integer.parse() do
+      {hours, _} -> {:ok, hours * 60}
+      _ -> {:error, "expected hours"}
+    end
   end
 
-  defp _parse_minutes(<<>>, string) do
-    {:ok, String.to_integer(string)}
+  defp convert_to_minutes(hours_string, minutes_string) do
+    hours = hours_string |> String.trim() |> Integer.parse()
+    minutes = minutes_string |> String.trim() |> Integer.parse()
+
+    case {hours, minutes} do
+      {{hours, _}, {minutes, _}} -> {:ok, hours * 60 + minutes}
+      _ -> {:error, "expected hours and minutes"}
+    end
   end
 
-  defp _parse_minutes(":", _tail, string) do
-    [hours, minutes] = String.split(string, ":")
-
-    {:ok, String.to_integer(hours) * 60 + String.to_integer(minutes)}
+  defp detect_format(<<>>, duration_string) do
+    case duration_string |> String.trim() |> Integer.parse() do
+      {minutes, ""} -> {:ok, minutes}
+      {_minutes, _} -> {:error, "unknown format"}
+      _ -> {:error, "expected minutes"}
+    end
   end
 
-  defp _parse_minutes("m", _tail, string) do
-    minutes = string |> String.trim() |> Integer.parse()
-
-    {:ok, minutes}
+  defp detect_format(<<head::binary-size(1), tail::binary>>, duration_string) do
+    parse_format(head, tail, duration_string)
   end
 
-  defp _parse_minutes(".", _tail, string) do
-    {hours, _} = string |> String.trim() |> Float.parse()
-    {:ok, Kernel.round(hours * 60)}
+  defp parse_format(":", _tail, duration_string) do
+    case String.split(duration_string, ":") do
+      ["", _] -> {:error, "expected hours"}
+      [_, ""] -> {:error, "expected minutes"}
+      [hours, minutes] -> convert_to_minutes(hours, minutes)
+    end
   end
 
-  defp _parse_minutes("h", _tail, string) do
-    [hours, minutesString] = String.split(string, "h")
-    {minutes, _} = _parse_minutes_string(minutesString)
-
-    {:ok, String.to_integer(hours) * 60 + minutes}
+  defp parse_format("m", _tail, duration_string) do
+    case duration_string |> String.trim() |> Integer.parse() do
+      {minutes, "m"} -> {:ok, minutes}
+      _ -> {:error, "expected minutes"}
+    end
   end
 
-  defp _parse_minutes(_, tail, string) do
-    _parse_minutes(tail, string)
+  defp parse_format(".", _tail, duration_string) do
+    case duration_string |> String.trim() |> Float.parse() do
+      {hours, ""} -> {:ok, Kernel.round(hours * 60)}
+      {hours, "h"} -> {:ok, Kernel.round(hours * 60)}
+      _ -> {:error, "expected 2 digits"}
+    end
   end
 
-  defp _parse_minutes_string("") do
-    {0, ""}
+  defp parse_format("h", _tail, duration_string) do
+    case String.split(duration_string, "h") do
+      ["", _] -> {:error, "expected hours"}
+      [hours, ""] -> convert_to_minutes(hours)
+      [hours, minutes] -> convert_to_minutes(hours, minutes)
+    end
   end
 
-  defp _parse_minutes_string(string) do
-    string |> String.trim() |> Integer.parse()
+  defp parse_format(_, tail, duration_string) do
+    detect_format(tail, duration_string)
   end
 end
